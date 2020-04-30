b794e5749399370b2f4b9d6dce21d94ea7a989c6
Valor <superpotpiehero@gmail.com>
2020-04-30 18:37:56 -0500

    merge current changes

--- practice-website/home.js
+++ practice-website/home.js
@@ -1,12 +1,3 @@
-let curTime = new Date()
-
-document.getElementById("curtime").innerHTML = curTime.toUTCString();
-
-if (curTime !== curTime.toUTCString()) {
-    document.getElementById("curtime").innerHTML = curTime.toUTCString();
-
-
-}
 
 
 
