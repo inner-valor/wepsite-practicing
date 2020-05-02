59f0385f9fd7f9dd65af31b4790035c55ee0daaa
Valor <superpotpiehero@gmail.com>
2020-05-01 19:44:39 -0500

    add sign-in message, change navbar margin. change main height

--- practice-website/home.js
+++ practice-website/home.js
@@ -1,3 +1,20 @@
 
 
 
+function whoAreYou() {
+    var person = prompt("please enter your name:");
+
+    if(person === "inner-valor") {
+        document.getElementById("personal").classList.remove("d-none");
+    } else {
+        document.getElementById("guest").classList.remove("d-none");
+    };
+}
+
+
+
+
+
+
+
+
